ec8a1386eabd973530a61939a3d5c081b858cd8f
Buiscki1 <122836250+Buiscki1@users.noreply.github.com>
2023-06-10 10:34:40 +0200

    Iteration 3-4

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountryDetails from './components/CountryDetails';
 import { Route, Routes } from 'react-router-dom';
-const countries = require('./countries.json');
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          'https://ih-countries-api.herokuapp.com/countries'
+        );
+        const data = await response.json();
+        setCountries(data);
+      } catch (error) {
+        console.log('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       <Navbar />
@@ -21,4 +39,5 @@ function App() {
     </div>
   );
 }
+
 export default App;
